1cc44d5ba352d022cbbc200656ecd611f7294ab9
Gera Shegalov <gera@apache.org>
2024-06-27 10:01:20 -0700

    Undo RapidsUDF change
    Signed-off-by: Gera Shegalov <gera@apache.org>

--- sql-plugin-api/src/main/java/com/nvidia/spark/RapidsUDF.java
+++ sql-plugin-api/src/main/java/com/nvidia/spark/RapidsUDF.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2024, NVIDIA CORPORATION.
+ * Copyright (c) 2020-2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,6 +20,30 @@ import ai.rapids.cudf.ColumnVector;
 
 /** A RAPIDS accelerated version of a user-defined function (UDF). */
 public interface RapidsUDF {
+  /**
+   * Evaluate a user-defined function with RAPIDS cuDF columnar inputs
+   * producing a cuDF column as output. The method must return a column of
+   * the appropriate type that corresponds to the type returned by the CPU
+   * implementation of the UDF (e.g.: INT32 for int, FLOAT64 for double,
+   * STRING for String, etc) or a runtime exception will occur when the
+   * results are marshalled into the expected Spark result type for the UDF.
+   * The number of rows of output must match the number of rows for the
+   * input arguments, which will all have the same row count.
+   * <p/>
+   * Note that the inputs should NOT be closed by this method, as they will
+   * be closed by the caller. This method must close any intermediate cuDF
+   * results produced during the computation (e.g.: `Table`, `ColumnVector`
+   * or `Scalar` instances).
+   * @param args columnar inputs to the UDF that will be closed by the caller
+   *             and should not be closed within this method.
+   * @return columnar output from the user-defined function
+   * @deprecated Use {@link #evaluateColumnar(int, ColumnVector...)}
+   */
+  @Deprecated
+  default ColumnVector evaluateColumnar(ColumnVector... args) {
+    throw new UnsupportedOperationException(
+        "Must override evaluateColumnar(int numRows, ColumnVector... args");
+  }
 
   /**
    * Evaluate a user-defined function with RAPIDS cuDF columnar inputs
@@ -40,5 +64,7 @@ public interface RapidsUDF {
    *             and should not be closed within this method.
    * @return columnar output from the user-defined function
    */
-   ColumnVector evaluateColumnar(int numRows, ColumnVector... args);
+  default ColumnVector evaluateColumnar(int numRows, ColumnVector... args) {
+    return evaluateColumnar(args);
+  }
 }
