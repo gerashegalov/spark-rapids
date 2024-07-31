a356af501207dd5dd13b7aa21ff9a359260ff01f
Gera Shegalov <gera@apache.org>
2024-07-30 23:27:12 -0700

    link first
    Signed-off-by: Gera Shegalov <gera@apache.org>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/RapidsConf.scala
@@ -96,7 +96,7 @@ object ConfHelper {
     val t = if (text != null) text else key
     // The anchor cannot be too long, so for now
     val a = key.replaceFirst("spark.rapids.", "")
-    s"""<a id="$a" class="an" href="#user-content-$a">$t</a>"""
+    s"""[(#)](#user-content-$a)<a id="$a"></a> `$t`"""
   }
 
   def getSqlFunctionsForClass[T](exprClass: Class[T]): Option[Seq[String]] = {
