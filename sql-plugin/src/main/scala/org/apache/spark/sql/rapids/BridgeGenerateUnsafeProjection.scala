d5f1a9d8f79a340b6bac8ee7140f16c80e0af7a2
Gera Shegalov <gshegalov@nvidia.com>
2026-06-04 18:06:33 -0700

    Unshim bridge unsafe projection generator

--- sql-plugin/src/main/scala/org/apache/spark/sql/rapids/BridgeGenerateUnsafeProjection.scala
+++ sql-plugin/src/main/scala/org/apache/spark/sql/rapids/BridgeGenerateUnsafeProjection.scala
@@ -262,8 +262,8 @@ private object BridgeUnsafeProjectionCodegen
  *
  * @note The returned UnsafeRow will be pointed to a scratch buffer inside the projection.
  */
-object BridgeGenerateUnsafeProjection extends
-  CodeGenerator[Seq[Expression], BridgeUnsafeProjection] {
+object BridgeGenerateUnsafeProjection {
+  private val codegenLog = org.slf4j.LoggerFactory.getLogger(getClass)
 
   case class Schema(dataType: DataType, nullable: Boolean)
 
@@ -571,6 +571,18 @@ object BridgeGenerateUnsafeProjection extends
   protected def bind(in: Seq[Expression], inputSchema: Seq[Attribute]): Seq[Expression] =
     bindReferences(in, inputSchema)
 
+  def newCodeGenContext(): CodegenContext = new CodegenContext
+
+  def generate(expressions: Seq[Expression]): BridgeUnsafeProjection = {
+    create(canonicalize(expressions))
+  }
+
+  def generate(
+      expressions: Seq[Expression],
+      inputSchema: Seq[Attribute]): BridgeUnsafeProjection = {
+    generate(bind(expressions, inputSchema))
+  }
+
   def generate(
       expressions: Seq[Expression],
       subexpressionEliminationEnabled: Boolean): BridgeUnsafeProjection = {
@@ -664,7 +676,9 @@ object BridgeGenerateUnsafeProjection extends
 
     val code = CodeFormatter.stripOverlappingComments(
       new CodeAndComment(codeBody, ctx.getPlaceHolderToComments()))
-    logDebug(s"code for ${expressions.mkString(",")}:\n${CodeFormatter.format(code)}")
+    if (codegenLog.isDebugEnabled) {
+      codegenLog.debug(s"code for ${expressions.mkString(",")}:\n${CodeFormatter.format(code)}")
+    }
 
     val (clazz, _) = CodeGenerator.compile(code)
     clazz.generate(ctx.references.toArray).asInstanceOf[BridgeUnsafeProjection]
